d71798af8667a3acdf484e5f8521b5b6913e7bc6
ghaithAlMasri <almasrighaith101@gmail.com>
2023-04-16 04:28:21 +0200

    added some spaces

--- App.py
+++ App.py
@@ -22,7 +22,7 @@ with st.sidebar:
     st.image('./logo-no-background.png')
     st.title('Easy ML Model Generator')
     choice = st.radio('Please go through the following steps sequentially:', ['Upload Data', 'Profile Data', 'Model Data', 'Download Model!'])
-    st.info('The easiest way to getting your pkl for Crpto/Stock regression problems!Download the historical data free at https://www.dukascopy.com/swiss/english/marketwatch/historical/. This was built using Streamlit, Pycaret, Pandas and more!')
+    st.info('The easiest way to getting your pkl for Crpto/Stock regression problems! Download the historical data free at https://www.dukascopy.com/swiss/english/marketwatch/historical/. This was built using Streamlit, Pycaret, Pandas and more!')
     st.write('@2023 GHAITH ALL RIGHTS RESERVED')
 
 
